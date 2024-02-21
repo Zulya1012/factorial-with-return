a6a6189dad0f25b8df3d5ee71ebf7f7a5f529730
Geydarova Zulfiya <152681616+Zulya1012@users.noreply.github.com>
2024-02-21 20:58:29 +0400

    Update fact2.java

--- fact2.java
+++ fact2.java
@@ -1,15 +1,14 @@
-public class fact2{
-  public static void main(String[] arg){
+public class fact2 {
+  public static void main(String[] arg) {
     factorial(10);
     System.out.println(factorial(10));
   }
-  public static int factorial(int x){
-    if (x==1){
+  public static int factorial(int x) {
+    if (x == 1) {
       return 10;
     }
-    
-  return x*factorial(x-1);
-  
-  
-}
+
+    return x * factorial(x - 1);
+
+  }
 }
